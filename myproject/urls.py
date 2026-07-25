d4f6a39f01cca741bffba2fcd2f74ec45f3ab4cd
Emamul khan <emamulk63@gmail.com>
2026-07-25 10:23:31 +0600

    Fix login urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,14 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 
-    path('', include('chat.urls')),
-    path('accounts/', include('accounts.urls')),
+    path("", include("chat.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
