4b4fe23df950db73293430bf8fd3214d3f6ab3af
Emamul khan <emamulk63@gmail.com>
2026-07-25 10:16:46 +0600

    Fix urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,22 +8,6 @@ urlpatterns = [
 
     path('', include('chat.urls')),
     path('accounts/', include('accounts.urls')),
-
-    path(
-        'login/',
-        auth_views.LoginView.as_view(
-            template_name='login.html'
-        ),
-        name='login'
-    ),
-
-    path(
-        'logout/',
-        auth_views.LogoutView.as_view(
-            next_page='/'
-        ),
-        name='logout'
-    ),
 ]
 
 if settings.DEBUG:
